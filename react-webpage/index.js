ff7795f0af9e8678f8af8a6bcebd8901a563d9df
Lize101 <miextr4@gmail.com>
2022-11-06 12:00:34 +0000

    Add ReactJS to HTML and CSS page

--- react-webpage/index.js
+++ react-webpage/index.js
@@ -5,25 +5,70 @@ function ClickableButton() {
   const [clicked, setClick] = React.useState(false);
 
   if (clicked) {
-    return 'Nothing here yet! Change this text to add more info.';
+    return React.createElement('h1', {className : "h1Style"}, 'Hello World');
   }
 
   return React.createElement(
     'button', {
-    style: {
-      backgroundColor: "#3392e4",
-      color: "#fff",
-      border: "0",
-      padding: "10px 20px",
-      fontWeight: "bold",
-      borderRadius: "5px",
-      fontSize: "1.1em"
-    },
+        className : "main-button",
+    // style: {
+    //   backgroundColor: "#3392e4",
+    //   color: "#fff",
+    //   border: "0",
+    //   padding: "10px 20px",
+    //   fontWeight: "bold",
+    //   borderRadius: "5px",
+    //   fontSize: "1.1em"
+    // },
     onClick: () => setClick(true)
   },
-    'Click To Reveal'
+    'Press Here'
   );
 }
 
 const root = ReactDOM.createRoot(document.querySelector('#root'));
 root.render(React.createElement(ClickableButton));
+
+
+
+
+
+
+
+
+
+// function ClickableButton() {
+
+//   const [clicked, setClick] = React.useState(false);
+
+//   if (clicked) {
+//     return 'Hello World!';
+//   }
+
+//   return React.createElement(
+//     'button', {
+//         image.png  style: {
+//       backgroundColor: "#3392e4",
+//       color: "#fff",
+//       border: "0",
+//       padding: "10px 20px",
+//       fontWeight: "bold",
+//       borderRadius: "5px",
+//       fontSize: "1.1em"
+//     },
+//     onClick: () => setClick(true)
+//   },
+//     'Press Here'
+//   );
+// }
+
+// const root = ReactDOM.createRoot(document.querySelector('#root'));
+// root.render(React.createElement(ClickableButton));
+
+// import React from 'react';
+// import ReactDOM from 'react-dom'; 
+
+// const name = 'Sarah';
+// const element = <h1>{name}'s React Page</h1>;
+
+// ReactDOM.render(element, document.getElementById('root'));
