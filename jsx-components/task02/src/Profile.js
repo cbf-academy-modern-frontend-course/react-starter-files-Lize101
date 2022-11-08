cc0219c4e61e6d64096887123467e8ddeebfb887
Lize101 <miextr4@gmail.com>
2022-11-08 21:36:47 +0000

    Add Profile function

--- jsx-components/task02/src/Profile.js
+++ jsx-components/task02/src/Profile.js
@@ -1,5 +1,18 @@
 
 /* Add the Profile function here */
-
+function Profile(props) {
+   if (props.name === "Liz") {
+        return <main id="content" role="main" className="base">
+            <h1>{props.name}’s React page</h1>
+            <p>{props.bio}</p>
+          </main>
+       }
+    else {
+      return <main id="content" role="main" className="base">
+        <h1>Just a React Page</h1>
+          <p>Nothing to see here!</p>
+        </main>    
+      }
+    }
 
 export default Profile
