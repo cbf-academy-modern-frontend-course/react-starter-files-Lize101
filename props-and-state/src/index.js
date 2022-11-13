492bcfcec07162dd425f3fe80e5817e0d8901a8e
Lize101 <miextr4@gmail.com>
2022-11-13 15:31:23 +0000

    Add counter unclick button

--- props-and-state/src/index.js
+++ props-and-state/src/index.js
@@ -4,15 +4,17 @@ import './index.css';
 //import data from './models/headphones.json';
 
 import GadgetList from './components/GadgetList';
+import ClickCounter from './components/ClickCounter'
 const mainStyle = {"fontFamily":"Arial"};
 
 const App = () => {
-  const headphones =  [{"title":"Headphones 1", "price":358.99},{"title":"Headphones 2", "price":378.99},{"title":"Headphones 3", "price":858.99},{"title":"Headphones 4", "price":358.00}, {"title":"Headphones 5", "price":228.99}];
+  const headphones =  [{"id":1, "title":"Headphones 1", "price":358.99},{"id":2, "title":"Headphones 2", "price":378.99},{"id":3, "title":"Headphones 3", "price":858.99},{"id":4, "title":"Headphones 4", "price":358.00}, {"id":5, "title":"Headphones 5", "price":228.99}];
   const [headphonesList] = useState(headphones);
 
   return <section style={mainStyle}>
     <h1>Electronics Store</h1>
     <GadgetList items={headphonesList}/>
+    <ClickCounter/>
   </section>;
 }
 
