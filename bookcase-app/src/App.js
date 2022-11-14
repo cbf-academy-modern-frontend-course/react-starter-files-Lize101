73652ed92cf7191386db546a5a44b4677886b9c3
Lize101 <miextr4@gmail.com>
2022-11-14 21:37:58 +0000

    Add book details to the page

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
-import Book from './components/Book';
-import data from './models/books.json';
+// import React from 'react';
+// import Book from './components/Book';
+// import data from './models/books.json';
+
+// function App() {
+//   const books = data;
+//   return <Book/>;
+// }
+
+// export default App;
+
+import React from "react";
+import Book from "./components/Book";
+import data from "./models/books.json";
+import { useState } from "react";
 
 function App() {
-  const books = data;
-  return <Book/>;
+  const [books] = useState(data);
+  // const books = data;
+  return books.map(book => <Book key={book.id} book={book}/>);
 }
 
 export default App;
-
