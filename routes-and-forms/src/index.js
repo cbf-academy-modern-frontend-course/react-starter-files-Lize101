00755599bd098ccccdbed61cf3ad0a80fc18a642
Lize101 <miextr4@gmail.com>
2022-11-15 19:54:40 +0000

    Add routes

--- routes-and-forms/src/index.js
+++ routes-and-forms/src/index.js
@@ -1,6 +1,10 @@
 import {React} from "react";
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom'
+import Home from "./components/Home";
+import Kittens from "./components/Kittens";
+import Puppies from "./components/Puppies";
+
 
 export default function App() {
   return (
@@ -8,31 +12,15 @@ export default function App() {
       <ul className="breadcrumb">
         <li><Link to="/"> All Pets </Link></li>
         <li><Link to="/kittens"> Kittens </Link></li>
+        <li><Link to="/puppies"> Puppies </Link></li>
       </ul>
       <Routes>
         <Route path="/" element={<Home/>} />
-        <Route path="kittens" element={<Kittens />} />
+        <Route path="/kittens" element={<Kittens />} />
+        <Route path="/puppies" element={<Puppies />} />
       </Routes>
   </Router>
   );
-}
-
-function Home() {
-  return <>
-          <h1>All Pets</h1>
-          <section>This is the page for all your pet needs</section>
-          <img src="imgs/kitten.jpg" alt="Kitten"/>
-          <img src="imgs/puppy.jpg" alt="Puppy"/>
-          <img src="imgs/duckling.jpg" alt="Duckling"/>
-        </>
-}
-
-function Kittens() {
-  return <>
-      <h1>Kittens</h1>
-      <section>This is the cats page</section>
-      <img src="imgs/kitten.jpg" alt="Kitten"/>
-    </>
 }
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
