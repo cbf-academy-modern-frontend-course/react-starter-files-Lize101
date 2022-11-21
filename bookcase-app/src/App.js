969a384d642e69a0c129cce21a0947fb6930ff5a
Lize101 <miextr4@gmail.com>
2022-11-21 16:24:35 +0000

    Update addBook function to display book titles in the console

--- bookcase-app/src/App.js
+++ bookcase-app/src/App.js
@@ -13,11 +13,29 @@ import React from "react";
 import Book from "./components/Book";
 import data from "./models/books.json";
 import { useState } from "react";
+// import Search from "./components/Search";
+// import {BrowserRouter as Router, Route} from "react-router-dom";
 
 function App() {
   const [books] = useState(data);
   // const books = data;
-  return books.map(book => <Book key={book.id} book={book}/>);
+  return (
+    <>
+    <h2>Liz's Bookcase App</h2>
+    {/* <Search/> */}
+    {books.map((book) => <Book key={book.id} book={book}/>)}
+  </>
+  )
 }
 
 export default App;
+
+{/* <Router>
+<Route exact path="/" element={
+  <>
+    <h2>Welcome to the Bookcase App</h2>
+    <Search/>
+    {books.map(book => <Book key={book.id} book={book}/>)}
+  </>
+}/>
+</Router> */}
---
+++ bookcase-app/src/components/Search.js
@@ -0,0 +1,13 @@
+// const Search = (props) => {
+
+//     return (
+//         <div>
+//             <form>
+//                 <input type="text" value={keyword} onChange={(e) =>setKeyword(e.target.value)} />
+//                 <button type="submit">Submit</button>
+//             </form>
+//         </div>
+//     )
+// }
+
+// export default Search
